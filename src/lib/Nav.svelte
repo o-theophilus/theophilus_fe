2496668af0f3c32990dbd22d19b22997521b2002
theophilus <theophilus.ogbolu@gmail.com>
2021-04-15 18:10:28 +0100

    try

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,15 +1,15 @@
-<script>
-	// import Link from './navLink.svelte';
-</script>
+<!-- <script>
+	import Link from './navLink.svelte';
+</script> -->
 
 
-<nav class="nav">
+<!-- <nav class="nav">
 	<ul class="nav__block">
-		<!-- <Link name={'home'} link={''} />
+		<Link name={'home'} link={''} />
 		<Link name={'about'} />
 		<Link name={'projects'} />
 		<Link name={'blog'} />
-		<Link name={'contact'} /> -->
+		<Link name={'contact'} />
 	</ul>
 </nav>
 
@@ -38,4 +38,8 @@
 		/* fix */
 		padding: 0;
 	}
-</style>
+</style> -->
+
+<p>
+	hello
+</p>
