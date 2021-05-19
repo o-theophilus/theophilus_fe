f6dc9b7581e2fe2faceaddaae35ac6b5ba14dadf
theophilus <theophilus.ogbolu@gmail.com>
2021-05-19 23:55:33 +0100

    mv

--- src/routes/del.svelte
+++ src/routes/del.svelte
@@ -16,14 +16,32 @@
 			title="container"
 		/>
 
-		<button id="autoRotate">Auto rotate</button>
-		<button id="autoRotateDirection">direction</button>
-		<input type="range" min="1" max="8" value="2" step="0.1" id="autoRotateSpeed" />
+		<div class="ui">
+			<div class="btn" id="autoRotate">AR</div>
+			<div class="btn" id="autoRotateDirection">d</div>
+			<input type="range" min="1" max="8" value="2" step="0.1" id="autoRotateSpeed" />
+		</div>
 	</div>
 </Content>
 
 <style>
-	:global(.v3d-annotation) {
-		display: none;
+	.block {
+		position: relative;
+	}
+	iframe {
+		/* background-color: black; */
+	}
+	.ui {
+		position: absolute;
+		display: flex;
+		gap: 20px;
+
+		top: 0;
+		left: 0;
+	}
+	.btn {
+		background-color: red;
+		width: 50px;
+		height: 50px;
 	}
 </style>
