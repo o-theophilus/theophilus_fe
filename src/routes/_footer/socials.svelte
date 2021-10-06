26c611a56f8dd5e8f634a3328398121156212205
Theophilus Oregie Ogbolu <theophilus.ogbolu@gmail.com>
2021-10-06 12:34:14 +0100

    change socials contact links

--- src/routes/_footer/socials.svelte
+++ src/routes/_footer/socials.svelte
@@ -6,10 +6,10 @@
 	<a href="https://wa.me/2348067397793/?text=Hello%20Theophilus">
 		<SVG type="whatsapp" />
 	</a>
-	<a href="https://m.facebook.com/Pheezie" target="_blank" rel="noopener noreferrer">
+	<a href="https://m.facebook.com/o.theophilus" target="_blank" rel="noopener noreferrer">
 		<SVG type="facebook" />
 	</a>
-	<a href="https://twitter.com/Pheezie" target="_blank" rel="noopener noreferrer">
+	<a href="https://twitter.com/o__theophilus" target="_blank" rel="noopener noreferrer">
 		<SVG type="twitter" />
 	</a>
 	<a
@@ -19,7 +19,7 @@
 	>
 		<SVG type="linkedin" />
 	</a>
-	<a href="https://github.com/pheezie" target="_blank" rel="noopener noreferrer">
+	<a href="https://github.com/o-theophilus" target="_blank" rel="noopener noreferrer">
 		<SVG type="github" />
 	</a>
 </div>
